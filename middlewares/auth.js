7513b6995915e719aa1cf96d3a0c90c5cbba6767
Alexey Popov <lepa1984@mail.ru>
2023-09-10 20:31:10 +0300

    настройка CORS

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,18 @@
 const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/AuthError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+  const extractToken = (header) => header.replace('jwt=', '');
+  if (!req.headers.cookie || !req.headers.cookie.startsWith('jwt=')) {
+    next(new AuthError('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer ', '');
+
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(extractToken(req.headers.cookie), 'unique-secret-key');
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new AuthError('Необходима авторизация'));
   }
 
   req.user = payload;
