4a543fd91b79823276d721da39f5e953502fd601
Alexey Popov <lepa1984@mail.ru>
2023-09-11 13:22:20 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const movieRoutes = require('./movies');
 const auth = require('../middlewares/auth');
 const NotFound = require('../errors/NotFound');
 
+const { createUser, login } = require('../controllers/users');
+const { validLogin, validCreateUser } = require('../middlewares/validation');
+
+router.post('/sign-in', validLogin, login);
+router.post('/sign-up', validCreateUser, createUser);
+router.use(auth);
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
 router.use('/*', auth, (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,7 @@
 const router = require('express').Router();
 const { userUpdateValidator } = require('../middlewares/validation');
 const { getUserInfo, updateUserInfo } = require('../controllers/users');
-const { createUser, login } = require('../controllers/users');
-const { validLogin, validCreateUser } = require('../middlewares/validation');
-const auth = require('../middlewares/auth');
 
-router.use('/sign-in', validLogin, login);
-router.use('/sign-up', validCreateUser, createUser);
-router.use(auth);
 router.get('/me', getUserInfo);
 router.patch('/me', userUpdateValidator, updateUserInfo);
 
