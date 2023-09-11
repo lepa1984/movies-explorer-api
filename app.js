3f998575106cf865c51ae959e59d025eca81b236
Alexey Popov <lepa1984@mail.ru>
2023-09-11 13:48:31 +0300

    cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const errorHandler = require('./middlewares/error-handler');
 const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 const limiter = rateLimit({
