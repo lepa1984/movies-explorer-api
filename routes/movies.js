b0ec9f0088fc06b2273ae1a60aa251f46282258b
Alexey Popov <lepa1984@mail.ru>
2023-09-12 22:31:53 +0300

    насстройка

--- routes/movies.js
+++ routes/movies.js
@@ -11,5 +11,5 @@ const {
 
 router.get('/', getMovies);
 router.post('/', moviesCreateValidator, createMovie);
-router.delete('/:movieId', moviesIdValidator, deleteMovie);
+router.delete('/:_id', moviesIdValidator, deleteMovie);
 module.exports = router;
