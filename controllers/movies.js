c5a6ea703f552780285e9c7973a5c62e060bff9a
Alexey Popov <lepa1984@mail.ru>
2023-09-12 23:40:47 +0300

    насстройка

--- controllers/movies.js
+++ controllers/movies.js
@@ -55,7 +55,7 @@ const deleteMovie = (req, res, next) => {
     .then((movie) => {
       if (movie.owner.toString() === req.user._id) {
         movie
-          .deleteOne(movie)
+          .remove()
           .then(() => res.send(movie))
           .catch(next);
       } else {
